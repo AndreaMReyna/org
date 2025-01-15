e3bcb9086d22ba7575e79e40a28e6a0ea24b7aea
AndreaMReyna <andi.mtzreyna@gmail.com>
2025-01-14 20:32:59 -0600

    intento 1 para que se cargen las imagenes

--- src/componentes/Footer/Footer.js
+++ src/componentes/Footer/Footer.js
@@ -4,16 +4,16 @@ const Footer = ( ) => {
     return  <footer className='footer'>
     <div className='redes'>
                <a href='https://smmd.org.mx/'>
-            <img src="/img/website.png" alt='website' />
+            <img src="/img/website.png" alt='Website' />
         </a>
         <a href='https://www.facebook.com/SMMDanza/?fref=ts'>
             <img src="/img/facebook.png" alt='Facebook' />
         </a>
         <a href='https://www.instagram.com/SMMDanza/'>
-            <img src="/img/instagram.png" alt='instagram' />
+            <img src="/img/instagram.png" alt='Instagram' />
         </a>
     </div>
-    <img className = "logo" src='/img/logo-footer.png' alt='org' />
+    <img className = "logo" src='/img/logo-footer.png' alt='Org' />
     <strong>Desarrollado por Andrea Martínez Reyna</strong>
 
 </footer>
